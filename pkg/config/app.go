e7f2f676d6b248555c9a8a5de7c32edce2761339
eichiarakaki <justanunusualperson@gmail.com>
2024-11-30 17:52:32 +0900

    Ensuring connectivity to the database & testing http requests.

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -2,14 +2,17 @@
 
 package config
 
-import "github.com/jinzhu/gorm"
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
 
 var (
 	db *gorm.DB
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "<username@>?charset=utf8&parseTime=True&loc=Local") // Opening a connection to the database
+	d, err := gorm.Open("mysql", "root:0101@tcp(127.0.0.1:3306)/bookstore?charset=utf8&parseTime=True&loc=Local") // Opening a connection to the database
 	if err != nil {
 		panic(err)
 	}
